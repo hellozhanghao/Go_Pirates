212807231ab2f2d43564cd34b6a59b7273a2b4fc
Zhang Hao <zhangghaoo@gmail.com>
2016-04-16 23:29:17 +0800

    add loading screen

--- core/src/com/go/gopirates/screen/LoadingScreen.java
+++ core/src/com/go/gopirates/screen/LoadingScreen.java
@@ -7,19 +7,15 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.go.gopirates.PirateGame;
 
 /**
- * Created by zhanghao on 3/4/16.
+ * Created by zhanghao on 16/4/16.
  */
-public class LoginScreen implements Screen {
-
+public class LoadingScreen implements Screen {
     Viewport viewport;
     OrthographicCamera cam;
 
@@ -27,47 +23,27 @@ public class LoginScreen implements Screen {
     private Sprite backgroundSprite;
     private Texture background;
 
-    private Image quickGameButton;
-    private Image joinRoomButton;
 
     private Stage stage;
     private PirateGame game;
     private boolean screenChanged;
 
-    public LoginScreen(PirateGame game) {
+    public LoadingScreen(PirateGame game) {
 
-        this.game=game;
-        batch=new SpriteBatch();
-        cam=new OrthographicCamera();
-        viewport=new FitViewport(1920f,1080f);
-        stage = new Stage(viewport,batch);
-        screenChanged=false;
+        this.game = game;
+        batch = new SpriteBatch();
+        cam = new OrthographicCamera();
+        viewport = new FitViewport(1920f, 1080f);
+        stage = new Stage(viewport, batch);
+        screenChanged = false;
     }
 
     @Override
     public void show() {
-        background=new Texture("img/MainScreen.png");
+        background = new Texture("img/LoadingScreen.png");
         background.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         backgroundSprite = new Sprite(background);
 
-        quickGameButton=new Image(new Texture("img/QuickGame.png"));
-        quickGameButton.setSize(600,200);
-        quickGameButton.setPosition(250, 600);
-        stage.addActor(quickGameButton);
-
-        quickGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.playServices.startQuickGame();
-                game.setScreen(new LoadingScreen(game));
-            }
-        });
-
-        joinRoomButton=new Image(new Texture("img/JoinRoom.png"));
-        joinRoomButton.setSize(600, 200);
-        joinRoomButton.setPosition(1050, 600);
-        stage.addActor(joinRoomButton);
-        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -82,7 +58,14 @@ public class LoginScreen implements Screen {
         stage.act();
         stage.draw();
 
-
+        if (game.sessionInfo.mState.equals("Play") & !screenChanged) {
+            screenChanged = true;
+            Gdx.app.log("Login", "ChangeScreen");
+            game.playServices.broadcastMessage("Hello World!");
+            Gdx.app.log("PirateGame", game.sessionInfo.mId);
+            game.sessionInfo.setUpGame();
+            game.setScreen(new PlayScreen(game));
+        }
     }
 
     @Override
@@ -109,5 +92,4 @@ public class LoginScreen implements Screen {
     public void dispose() {
         stage.dispose();
     }
-
 }
